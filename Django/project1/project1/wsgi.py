10f0f78ac4aecb1e1cad882e69e4eec63b902101
Liyang Han <artanisinyok@hotmail.com>
2020-01-10 17:24:28 +0800

    Fix

--- Django/project1/project1/wsgi.py
+++ Django/project1/project1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for personal_aviation_model_database project.
+WSGI config for project1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_aviation_model_database.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1.settings')
 
 application = get_wsgi_application()
