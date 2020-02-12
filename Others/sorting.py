6643ac658d5eb48d77df6723e9509167821d2cea
Liyang Han <artanisinyok@hotmail.com>
2020-02-12 11:18:39 +0800

    bubbleSort

--- Others/sorting.py
+++ Others/sorting.py
@@ -1,41 +1,80 @@
-k = [7, 4, 1, 5, 2, 8, 0, 3, 6, 9]
+class Sorting:
+
+    def __init__(self, array):
+        self.array = array
+
+    def straightInsertionSortAscending(self):
+        '''
+        直接插入排序升序
+        '''
+        for i in range(1, len(self.array)):
+            if self.array[i] < self.array[i - 1]:
+                temp = self.array[i]
+                j = i - 1
+            while self.array[j] > temp and j >= 0:
+                self.array[j + 1] = self.array[j]
+                j -= 1
+            self.array[j + 1] = temp
+        print(self.array)
+
+    def straightInsertionSortDescending(self):
+        '''
+        直接插入排序降序
+        '''
+        for i in range(1, len(self.array)):
+            if self.array[i] > self.array[i - 1]:
+                temp = self.array[i]
+                j = i - 1
+            while self.array[j] < temp and j >= 0:
+                self.array[j + 1] = self.array[j]
+                j -= 1
+            self.array[j + 1] = temp
+        print(self.array)
+
+    def bubbleSortAscending(self):
+        '''
+        冒泡排序升序
+        '''
+        for i in range(1, len(self.array)):
+            flag = 0
+            for j in range(len(self.array) - i):
+                if self.array[j] > self.array[j + 1]:
+                    temp = self.array[j + 1]
+                    self.array[j + 1] = self.array[j]
+                    self.array[j] = temp
+                    flag = 1
+                j += 1
+            i += 1
+            if flag == 0:
+                break
+        print(self.array)
+
+    def bubbleSortDescending(self):
+        '''
+            冒泡排序降序
+            '''
+        for i in range(1, len(self.array)):
+            flag = 0
+            for j in range(len(self.array) - i):
+                if self.array[j] < self.array[j + 1]:
+                    temp = self.array[j + 1]
+                    self.array[j + 1] = self.array[j]
+                    self.array[j] = temp
+                    flag = 1
+                j += 1
+            i += 1
+            if flag == 0:
+                break
+        print(self.array)
+    
+    def quickSortAscending(self):
+        '''
+        快速排序升序
+        '''
 
 
-def straightInsertionSortAscending(array):
-    '''
-    直接插入排序升序
-    '''
-    for i in range(1, len(k)):
-        if k[i] < k[i - 1]:
-            temp = k[i]
-            j = i - 1
-        while k[j] > temp and j >= 0:
-            k[j + 1] = k[j]
-            j -= 1
-        k[j + 1] = temp
-    print(k)
-
-
-def straightInsertionSortDescending(array):
-    '''
-    直接插入排序降序
-    '''
-    for i in range(1, len(k)):
-        if k[i] > k[i - 1]:
-            temp = k[i]
-            j = i - 1
-        while k[j] < temp and j >= 0:
-            k[j + 1] = k[j]
-            j -= 1
-        k[j + 1] = temp
-    print(k)
-
-
-def bubbleSortAscending(array):
-    '''
-    冒泡排序升序
-    '''
-
-
-straightInsertionSortAscending(k)
-straightInsertionSortDescending(k)
+x = Sorting([4, 2, 3, 6, 5, 1, 9, 7, 0, 8])
+x.straightInsertionSortAscending()
+x.straightInsertionSortDescending()
+x.bubbleSortAscending()
+x.bubbleSortDescending()
